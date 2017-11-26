3d9764359d3abaa49bcca22e03e2b94ba6c32f11
Murilo Trigo <murilotrigopaz@gmail.com>
2017-11-26 17:10:12 -0500

    Fixes issues where the absolute value of the diffences had to be considered insted of the difference

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var x = undefined;
 var y = undefined;
-const THRESHOLD = 50;
+const THRESHOLD = 100;
 window.onload = () => {
 	var content = document.getElementById('content');
 	content.addEventListener('touchstart', (e) => {
@@ -8,11 +8,12 @@ window.onload = () => {
 		y = e.touches[0].clientY;
 	}, false);
 	content.addEventListener('touchend', (e) => {
-		return;
 		let dx = e.changedTouches[0].clientX - x;
 		let dy = e.changedTouches[0].clientY - y;
-		if (dy > dx) return;
-		if (dx < THRESHOLD) { document.getElementById('fwd').click();}
-		else if (dx > THRESHOLD) { document.getElementById('back').click(); }
+		let adx = Math.abs(dx);
+		let ady = Math.abs(dy);
+		if (ady > adx || adx < THRESHOLD) return;
+		if (dx < 0) { document.getElementById('fwd').click();}
+		else { document.getElementById('back').click(); }
 	}, false); 
 }
